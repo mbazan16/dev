0b3b34e86b96a5c978d297fddb6763c356966cfe
formacion <formacion@ITEM-81712.emea.msad.sopra>
2020-02-04 18:01:39 +0100

    Cambios en la configuracion de log4j

--- TutorialJPA/src/Programa.java
+++ TutorialJPA/src/Programa.java
@@ -2,14 +2,17 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Programa {
+import org.apache.log4j.Logger;
 
+public class Programa {
+	static Logger log = Logger.getLogger(Programa.class.getName());
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("TutorialJPA");
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		Animal hipo = DAOAnimal.crearAnimal(em,"Hippo","Hippopotamus amphibius", "Mamífero", "Sabana");
 		System.out.println(hipo);
+		em.getTransaction().commit();
 		
 		
 	}
